04692ce895ff7a99b7dc2af9bad32b04c9af8768
Chris Lyon <wyleus@gmail.com>
2024-05-01 19:24:33 +0100

    Sanbells in SVG

---
+++ changes/bells/static/bells/pagesize.js
@@ -0,0 +1,8 @@
+( function(){
+
+    const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    const height = window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight;
+
+    console.log(width, height);
+
+})();
