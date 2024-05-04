271fcb468cf9117593f4ff46e35beed6036a07ac
Chris Lyon <wyleus@gmail.com>
2024-05-04 18:48:02 +0100

    Working again

--- startMidori.sh
+++ startMidori.sh
@@ -4,4 +4,4 @@ xset s off       # disable screen saver
 xset s noblank    # don't blank the video device
 matchbox-window-manager &
 cd /home/wyleu/Sandbells
-midori -e fullscreen -a http://sandbells.local
+midori -e fullscreen -a http://sandbells.local/home/
