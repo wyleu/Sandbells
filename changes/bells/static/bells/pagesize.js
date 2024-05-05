a7af7d745450de11c516eed9bd2fd2d2107bc991
Chris Lyon <wyleus@gmail.com>
2024-05-05 18:10:31 +0100

    Height Width indication

--- changes/bells/static/bells/pagesize.js
+++ changes/bells/static/bells/pagesize.js
@@ -5,4 +5,21 @@
 
     console.log(width, height);
 
+
+
+
+
+function reportWindowSize() {
+
+    var width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var height = window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight;
+
+    d3.select('#height_value')
+    .text(height);
+    d3.select('#width_value')
+    .text(width);
+      };
+
+reportWindowSize();
+window.onresize = reportWindowSize
 })();
