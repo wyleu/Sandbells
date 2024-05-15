85b40bf8a045c4437eeb773784d4cc149fb97ff2
Chris Lyon <wyleus@gmail.com>
2024-05-15 18:04:19 +0100

    change to sandbells.sh

--- sandbells.sh
+++ sandbells.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 cd /home/wyleu/Sandbells
 source Bellvirtenv/bin/activate
+pip install -r requirements.txt
 cd changes
-python manage.py --noinput 
-collectstatic
+chmod 777 /var/www/html/static/bells/*.*
+python manage.py --noinput collectstatic
+chmod 777 /var/www/html/static/bells/*.*
 systemctl restart nginx.service
 systemctl restart midori-fullscreen.service
 systemctl restart gunicorn.service
