ae677b68fb7899ba03156dbae7f02d4b8d21cb78
Chris Lyon <wyleus@gmail.com>
2024-05-18 21:35:00 +0100

    PRess button TD's and Buttons

--- changes/static/js/apps.js
+++ changes/static/js/apps.js
@@ -1,35 +1,3 @@
-// const drums =new Howl({
-//     "src": [
-//         "/static/drums/drums.webm",
-//         "/static/drums/drums.mp3"
-//       ],
-//       "sprite": {
-//         "clap": [
-//           0,
-//           734.2630385487529
-//         ],
-//         "closed-hihat": [
-//           2000,
-//           445.94104308390035
-//         ],
-//         "crash": [
-//           4000,
-//           1978.6848072562354
-//         ],
-//         "kick": [
-//           7000,
-//           553.0839002267571
-//         ],
-//         "open-hihat": [
-//           9000,
-//           962.7664399092968
-//         ],
-//         "snare": [
-//           11000,
-//           354.48979591836684
-//         ]
-//       }
-// });
 
 const bells = new Howl(
   {
@@ -48,35 +16,104 @@ const bells = new Howl(
   }
 );
 
+
+
 const drumkit = document.querySelector('.drumkit');
+// const change = document.querySelector('.frontpage_table_display');
 
-// function playDrum(event){
-//   if (event.target.classList.contains('pad')){
-//     event.preventDefault();     /* prevent double click  */
-//     let soundToPlay = event.target.dataset.sound;
-//     drums.play(soundToPlay);
-//   }
-// }
 
-function playDrum(event){
-  if (event.target.classList.contains('pad')){
-    event.preventDefault();     /* prevent double click  */
-    let soundToPlay = event.target.dataset.sound;
-    bells.play(soundToPlay);
+ function playDrum(event){
+   if (event.target.classList.contains('pad')){
+     event.preventDefault();     /* prevent double click  */
+     let soundToPlay = event.target.dataset.sound;
+     bells.play(soundToPlay);
+   }
+ }
+
+function handleEvent(event) {
+  if (event.type === "mousedown") {
+    console.log('Ouch!', event);
+    var target = event.target;
+    var changebody = target.querySelectorAll(".changebody");
+    console.log(changebody.innerText);
+
+  } else {
+    console.log('Stroke!', event.type);
   }
 }
 
-function setViewportHeight(){
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
+function playString(event){
+  if (event.target.classList.contains('changebody')){
+    let bellstring = event.target.innerText;
+  }
+} 
 
-setViewportHeight();
-window.addEventListener('resize', () => {
-  setTimeout(setViewportHeight, 100);
-});
+// function setViewportHeight(){
+//   let vh = window.innerHeight * 0.01;
+//   document.documentElement.style.setProperty('--vh', `${vh}px`);
+// }
+
+// setViewportHeight();
+// window.addEventListener('resize', () => {
+//   setTimeout(setViewportHeight, 100);
+// });
 
 drumkit.addEventListener('click', playDrum);
 drumkit.addEventListener('touchstart', playDrum);   /* ipad  */
 
+//  change.addEventListener('mousedown', handleEvent);
+//  change.addEventListener('click', handleEvent);
+//  change.addEventListener('touchstart', handleEvent);   /* ipad  */
 
+// const table = document.getElementsByClassName('frontpage_table_display')[0];
+// const rows = table.getElementsByTagName('tr');
+// console.log(rows);
+
+// Array.from(rows).forEach((row, index) => {
+//   console.log('Adding Events', index);
+//   row.addEventListener('click', () =>{
+//     const cells = row.getElementsByTagName('td');
+//     const content1 = cells[1].innerText;
+//     console.log(content1);
+//   })
+// });
+
+// const table_element = document.querySelector('.frontpage_table_display');
+// table_element.addEventListener("click", tableElementClick, false);
+
+// function tableElementClick(event) {
+//   console.log("Turning it off...");
+//   event.preventDefault();
+// }
+
+
+// const rows = document.querySelectorAll('#frontpage_table_display_1 button');
+// console.log(rows); // 👉️ NodeList(3) [tr, tr, tr]
+
+// Array.from(rows).forEach((row, index) => {
+//   console.log(row, index);
+//   row.addEventListener('onmousedown', handleEvent);
+// });
+
+
+// $('#frontpage_table_display_1').on('click', 'tr', function(e){
+//   tableText($(this).html());
+// });
+
+// function tableText(tableRow) {
+//   var myJSON = JSON.stringify(tableRow);
+//   console.log(myJSON);
+// }
+
+
+
+// d3.selectAll('.frontpage_table_display')
+//     .on("click", (event)=> console.log(this));
+
+d3.selectAll(".ring_button")
+   .on("click", function(d, i ,e) {
+       console.log("BANG!!!!!:-", this.getAttribute("dataring"));
+   }
+);
+
+console.log('Run Thru');
