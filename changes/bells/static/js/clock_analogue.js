95d0d20a64b1d3cb479d1339c5909cfde9771c59
Chris Lyon <wyleus@gmail.com>
2026-06-20 16:46:55 +0100

    Power Supply problems ! Doh Clock has numbers & ticks

--- changes/bells/static/js/clock_analogue.js
+++ changes/bells/static/js/clock_analogue.js
@@ -1,5 +1,40 @@
 // Robust Clock for Midori Kiosk
 (function() {
+    function degreesToRadians(degrees){
+	  return degrees * (Math.PI/180);
+    }
+
+    function getCoordsByDegree(theta){
+		var radius = 130;
+		return { x : radius * Math.sin(degreesToRadians(theta)), y : radius * Math.cos(degreesToRadians(theta)) }
+    }
+
+    function createHourLabels(){
+	var arr = [6,5,4,3,2,1,12,11,10,9,8,7];
+
+        d3.select(".numbers").selectAll("text").data(arr).enter().append("text")
+         	.attr("x", function(d,i) { return getCoordsByDegree(i * 30).x + 175 - 6 ; })
+         	.attr("y", function(d,i) { return getCoordsByDegree(i * 30).y + 175 + 5; })
+         	.text(function(d) { return d; });
+    }
+
+    function drawMarks(){
+	var clockMarks = [];
+
+	for (var i = 1; i <= 61; i++) {
+	   i % 5 != 0 ? clockMarks.push(i) : "";
+	}
+
+	d3.select(".marks").selectAll("text").data(clockMarks).enter()
+		.append("text")
+		.attr("x", "175")
+		.attr("y", "175")
+		.attr("dy", "-128")
+		.attr("transform", function(d) { var degree = d * 6; return "rotate("+degree+" 175 175)" })
+		.text("|");
+    }
+
+
     function updateClock() {
         const now = new Date();
         const h = now.getHours() % 12;
@@ -16,6 +51,10 @@
         updateClock();
         setInterval(updateClock, 1000);
     }
+
+
+    createHourLabels();
+    drawMarks();
 
     window.addEventListener('load', startClock);
     document.addEventListener('DOMContentLoaded', startClock);
